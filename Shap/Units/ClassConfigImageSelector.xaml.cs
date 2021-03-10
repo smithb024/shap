2a45b13710ed864e6fc1d48cb50acf4262ded5a8
Bert Smith <bertram.smith@gmail.com>
2021-03-10 00:11:17 +0000

    #6 Adda config image selector

---
+++ Shap/Units/ClassConfigImageSelector.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Shap.Units
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for ClassConfigImageSelector.xaml
+    /// </summary>
+    public partial class ClassConfigImageSelector : UserControl
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="ClassConfigImageSelector"/> class.
+        /// </summary>
+        public ClassConfigImageSelector()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
