bbf77ee54c2d8aef6f1966ec380e2f10ec2c7666
Bert Smith <bertram.smith@gmail.com>
2021-09-02 23:20:02 +0100

    #14 Draw a graph

--- Shap/Units/VehicleDataWindow.xaml.cs
+++ Shap/Units/VehicleDataWindow.xaml.cs
@@ -1,28 +1,40 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace Shap.Units
+﻿namespace Shap.Units
 {
-  /// <summary>
-  /// Interaction logic for VehicleDataWindow.xaml
-  /// </summary>
-  public partial class VehicleDataWindow : Window
+    using System.Collections.Generic;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for VehicleDataWindow.xaml
+    /// </summary>
+    public partial class VehicleDataWindow : Window
   {
-    public VehicleDataWindow()
-    {
-      InitializeComponent();
+        public VehicleDataWindow()
+        {
+            this.InitializeComponent();
+            this.SetupChartProperties();
+        }
+
+        private void SetupChartProperties()
+        {
+            IEnumerable<string> titles = new List<string>
+            {
+                "Point1",
+                "Point2",
+                "Point3",
+                "Point4",
+                "Point5",
+            };
+            IEnumerable<double> values = new List<double>
+            {
+                10,
+                5,
+                15,
+                28,
+                2
+            };
+
+            this.chart1.Series[0].Points.DataBindXY(titles, values);
+            this.chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+        }
     }
-  }
 }
