920c240488adc43c51d6991c2548f2886af23f9e
Bert Smith <bertram.smith@gmail.com>
2022-03-28 21:48:07 +0100

    #31: Serialise Operators

---
+++ Shap/Common/SerialiseModel/Operator/OperatorDetails.cs
@@ -0,0 +1,25 @@
+﻿namespace Shap.Common.SerialiseModel.Operator
+{
+    using System.Collections.Generic;
+    using System.Xml.Serialization;
+
+    /// <summary>
+    /// Class used to serialise and deserialise the operator details.
+    /// </summary>
+    [XmlRoot("OperatorDetails")]
+    public class OperatorDetails
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="OperatorDetails"/> class.
+        /// </summary>
+        public OperatorDetails()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the Collection of operators.
+        /// </summary>
+        [XmlElement("Operators")]
+        public List<SingleOperator> Operators { get; set; }
+    }
+}
