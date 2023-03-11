f66c0a5fe42b52d075f57ffeeef106e41728df21
Bert Smith <bertram.smith@gmail.com>
2023-03-11 18:05:52 +0000

    #42: Add instructions.

--- Shap/Locations/Model/LocationAnalyser.cs
+++ Shap/Locations/Model/LocationAnalyser.cs
@@ -36,5 +36,46 @@
         {
 
         }
+
+        // Loop through each location.
+
+        // For each location:
+        // Create new lists for trips, classes and years.
+        // Create empty to and from counts.
+        // Open the LocationsDetail class for the location.
+        // Loop through each year.
+
+        // For each year:
+        // Create a LocationYear object.
+        // Loop through each month.
+
+        // For each month:
+        // Open the month file.
+        // Loop through each entry in the month file:
+
+        // For each entry:
+        // If the location is not present then return.
+        // Add a count to the to and from count.
+        // Add a count to the LocationYear.
+        // Create and add a trip object. Add it to the list and remove the oldest one if there are now more than 10.
+        // Loop through each unit.
+
+        // For each unit:
+        // Determine the type. 
+        // If this is the first of the type in the entry:
+        // If type is present in the type list:
+        // Add a count to the specific type.
+        // Else:
+        // Create a type object and include the type.
+        // Add the type to the type list.
+
+        // For each year:
+        // Add the LocationYear object to the list.{If the count is zero and there is nothing in the list don't add, this will prevent leading zeros}
+
+        // For each location:
+        // Overwrite the trips classes and years collections.
+        // Update the to and from counters {could be done along the way}
+        // Save the location.
+        // Send a location updated message?
     }
 }
