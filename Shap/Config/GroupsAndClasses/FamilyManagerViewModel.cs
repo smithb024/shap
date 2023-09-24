30329cba3283c295d7171088b325f51ef43be4bd
Bert Smith <bertram.smith@gmail.com>
2023-09-24 16:18:23 +0100

    #63: Create member

--- Shap/Config/GroupsAndClasses/FamilyManagerViewModel.cs
+++ Shap/Config/GroupsAndClasses/FamilyManagerViewModel.cs
@@ -2,11 +2,15 @@
 {
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Runtime.Remoting.Messaging;
     using System.Windows.Input;
+    using CommunityToolkit.Mvvm.Messaging;
     using NynaeveLib.Commands;
     using NynaeveLib.ViewModel;
     using Shap.Common.SerialiseModel.Family;
     using Shap.Interfaces.Io;
+    using Shap.Types;
 
     /// <summary>
     /// View model which manages the ability to add families
@@ -29,6 +33,11 @@
         private FamilyDetails serialisedFamilies;
 
         /// <summary>
+        /// Gets the index of the currently selected group.
+        /// </summary>
+        private int groupsIndex;
+
+        /// <summary>
         /// Initialises a new instance of the <see cref="FamilyManagerViewModel"/> class.
         /// </summary>
         /// <param name="ioControllers">IO Controllers</param>
@@ -38,6 +47,7 @@
             this.ioControllers = ioControllers;
             this.serialisedFamilies = this.ioControllers.Family.Read();
             this.Families = new ObservableCollection<string>();
+            this.MemberGroups = new ObservableCollection<string>();
 
             if (this.serialisedFamilies != null)
             {
@@ -52,7 +62,27 @@
                 this.serialisedFamilies.Families = new List<SingleFamily>();
             }
 
+            List<GroupsType> groupsCollection = ioControllers.Gac.LoadFile();
+            this.Groups =
+                new ObservableCollection<string>()
+                {
+                    string.Empty
+                };
+            this.groupsIndex = 0;
+
+            if (groupsCollection != null)
+            {
+                foreach (GroupsType groupType in groupsCollection)
+                {
+                    this.Groups.Add(groupType.Name);
+                }
+            }
+
             this.AddFamily = new CommonCommand(this.Add, this.CanAdd);
+            this.AddMember = new CommonCommand(this.AddMemberToFamily, () => true);
+            this.Messenger.Register<GroupsListMessage>(
+                this,
+                (r, message) => this.NewGroupsList(message));
         }
 
         /// <summary>
@@ -83,9 +113,33 @@
         public ObservableCollection<string> Families { get; }
 
         /// <summary>
+        /// Gets the names of all the available groups/classes.
+        /// </summary>
+        public ObservableCollection<string> Groups { get; private set; }
+
+        /// <summary>
+        /// Gets all the groups which are a member of the current family.
+        /// </summary>
+        public ObservableCollection<string> MemberGroups { get; private set; }
+
+        /// <summary>
+        /// Gets the index of the currently selected group/class.
+        /// </summary>
+        public int GroupsIndex
+        {
+            get => this.groupsIndex;
+            set => this.SetProperty(ref this.groupsIndex, value);
+        }
+
+        /// <summary>
         /// Add an item.
         /// </summary>
-        public ICommand AddFamily{ get; private set; }
+        public ICommand AddFamily { get; private set; }
+
+        /// <summary>
+        /// Add a member to the family.
+        /// </summary>
+        public ICommand AddMember { get; private set; }
 
         /// <summary>
         /// Save the updates.
@@ -127,6 +181,42 @@
         private bool CanAdd()
         {
             return !string.IsNullOrEmpty(this.Family);
+        }
+
+        /// <summary>
+        /// Add the selected group to the member groups property.
+        /// </summary>
+        public void AddMemberToFamily()
+        {
+            if (this.MemberGroups.Contains(this.Groups[this.GroupsIndex]))
+            {
+                return;
+            }
+
+            this.MemberGroups.Add(this.Groups[this.GroupsIndex]);
+            this.MemberGroups = new ObservableCollection<string>(this.MemberGroups.OrderBy(i => i));
+            this.OnPropertyChanged(nameof(this.MemberGroups));
+        }
+
+        /// <summary>
+        /// A new Groups List message have been received via the messenger.
+        /// </summary>
+        /// <param name="message">The message</param>
+        private void NewGroupsList(GroupsListMessage message)
+        {
+            this.GroupsIndex = 0;
+            this.Groups =
+                new ObservableCollection<string>
+                {
+                    string.Empty
+                };
+
+            foreach (string name in message.Groups)
+            {
+                this.Groups.Add(name);
+            }
+
+            this.OnPropertyChanged(nameof(this.Groups));
         }
     }
 }
