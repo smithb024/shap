fb35eeefb1917ac53fd4205b0107db30267aceae
smithb024 <bertram.smith@gmail.com>
2024-10-14 22:41:37 +0100

    #76 Set up a messenger

--- Shap/Feedback/ViewModels/FeedbackViewModel.cs
+++ Shap/Feedback/ViewModels/FeedbackViewModel.cs
@@ -1,11 +1,28 @@
 ﻿namespace Shap.Feedback.ViewModels
 {
     using NynaeveLib.ViewModel;
+    using Shap.Messages;
+    using NynaeveMessenger = NynaeveLib.Messenger.Messenger;
 
     /// <summary>
-    /// 
+    /// The view model which supports the feedback view.
     /// </summary>
     public class FeedbackViewModel : ViewModelBase
     {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="FeedbackViewModel"/> class.
+        /// </summary>
+        public FeedbackViewModel()
+        {
+            NynaeveMessenger.Default.Register<FeedbackMessage>(this, this.RunPrimaryMessage);
+        }
+
+        /// <summary>
+        /// A <see cref="FeedbackMessage"/> message has been recived from the messenger.
+        /// </summary>
+        /// <param name="message">The message</param>
+        private void RunPrimaryMessage(FeedbackMessage message)
+        {
+        }
     }
 }
