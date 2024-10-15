94d071bbe694278ec3d4a617a3e45cc133724612
smithb024 <bertram.smith@gmail.com>
2024-10-15 22:47:21 +0100

    #76: Setup the feedback view model

---
+++ Shap/Feedback/ViewModels/FeedbackRowViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace Shap.Feedback.ViewModels
+{
+    using NynaeveLib.ViewModel;
+    using Shap.Types.Enum;
+
+    /// <summary>
+    /// View model which supports a single row on the feedback window.
+    /// </summary>
+    public class FeedbackRowViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="FeedbackRowViewModel"/> class.
+        /// </summary>
+        /// <param name="priority">The feedback priority</param>
+        /// <param name="message">The feedback message</param>
+        public FeedbackRowViewModel(
+            FeedbackType priority,
+            string message)
+        {
+            this.Priority = priority;
+            this.Message = message;
+        }
+
+        /// <summary>
+        /// Gets the priority of the feedback.
+        /// </summary>
+        public FeedbackType Priority { get; }
+
+        /// <summary>
+        /// Gets the message to feedback.
+        /// </summary>
+        public string Message { get; }
+    }
+}
--- Shap/Feedback/ViewModels/FeedbackViewModel.cs
+++ Shap/Feedback/ViewModels/FeedbackViewModel.cs
@@ -2,6 +2,7 @@
 {
     using NynaeveLib.ViewModel;
     using Shap.Messages;
+    using System.Collections.ObjectModel;
     using NynaeveMessenger = NynaeveLib.Messenger.Messenger;
 
     /// <summary>
@@ -14,15 +15,37 @@
         /// </summary>
         public FeedbackViewModel()
         {
+            this.Rows = new ObservableCollection<FeedbackRowViewModel>();
             NynaeveMessenger.Default.Register<FeedbackMessage>(this, this.RunPrimaryMessage);
         }
 
         /// <summary>
-        /// A <see cref="FeedbackMessage"/> message has been recived from the messenger.
+        /// Gets the collection of rows to display on the view model.
         /// </summary>
+        public ObservableCollection<FeedbackRowViewModel> Rows { get; set; }
+
+        /// <summary>
+        /// A <see cref="FeedbackMessage"/> message has been recived from the messenger.
+        /// Insert it at the top of the feedback window.
+        /// </summary>
+        /// <remarks>
+        /// The feedback window only supports 10 rows, therefore if this results in 11 rows, ensure
+        /// that the oldest one is deleted.
+        /// </remarks>
         /// <param name="message">The message</param>
         private void RunPrimaryMessage(FeedbackMessage message)
         {
+            if (this.Rows.Count == 10)
+            {
+                this.Rows.RemoveAt(9);
+            }
+
+            FeedbackRowViewModel newRow =
+                new FeedbackRowViewModel(
+                    message.Priority,
+                    message.Message);
+
+            this.Rows.Insert(0, newRow);
         }
     }
 }
