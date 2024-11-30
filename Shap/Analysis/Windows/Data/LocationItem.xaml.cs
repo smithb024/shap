33e96538f4bb5cd8730fb9b51e274e3564b195b3
smithb024 <bertram.smith@gmail.com>
2024-11-30 22:51:57 +0000

    #70: Add index column to totals counter

--- Shap/Analysis/Windows/Data/LocationItem.xaml.cs
+++ Shap/Analysis/Windows/Data/LocationItem.xaml.cs
@@ -1,28 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Shap.Analysis.Windows.Data
+﻿namespace Shap.Analysis.Windows.Data
 {
+    using System.Windows.Controls;
+
     /// <summary>
     /// Interaction logic for LocationItem.xaml
     /// </summary>
     public partial class LocationItem : UserControl
     {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="LocationItem"/> class.
+        /// </summary>
         public LocationItem()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
