3498f21f6c3cd6245c107bbe760d3b8f4c5b2610
smithb024 <bertram.smith@gmail.com>
2024-11-30 23:04:17 +0000

    #70: Add index column to annual counter

--- Shap/Analysis/Windows/Data/FullYearItem.xaml.cs
+++ Shap/Analysis/Windows/Data/FullYearItem.xaml.cs
@@ -1,28 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Shap.Analysis.Windows.Data
+﻿namespace Shap.Analysis.Windows.Data
 {
+    using System.Windows.Controls;
+
     /// <summary>
     /// Interaction logic for FullYearItem.xaml
     /// </summary>
     public partial class FullYearItem : UserControl
     {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="FullYearItem"/> class.
+        /// </summary>
         public FullYearItem()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
