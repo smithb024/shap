27b9aef52cc7313c3103d27877ad855620da5ec6
smithb024 <bertram.smith@gmail.com>
2026-01-06 22:15:26 +0000

    #95: Refactor

--- Shap/Common/Commands/CommonCommand.cs
+++ Shap/Common/Commands/CommonCommand.cs
@@ -3,73 +3,65 @@
 /// </summary>
 namespace Shap.Common.Commands
 {
-  using System;
-  using System.Windows.Input;
+    using System;
+    using System.Windows.Input;
 
-  public class CommonCommand : ICommand
-  {
-    public CommonCommand(Action command)
-      : this (command, CanAlwaysRunCommand)
+    public class CommonCommand : ICommand
     {
-    }
+        public CommonCommand(Action command)
+          : this(command, CanAlwaysRunCommand)
+        {
+        }
 
-    public CommonCommand(
-      Action command,
-      Func<bool> canExecuteCommand)
-    {
-      this.RunCommand = command;
-      this.CanRunCommand = canExecuteCommand;
-    }
+        public CommonCommand(
+          Action command,
+          Func<bool> canExecuteCommand)
+        {
+            this.RunCommand = command;
+            this.CanRunCommand = canExecuteCommand;
+        }
 
-    /// <summary>
-    /// Run command action.
-    /// </summary>
-    public Action RunCommand
-    {
-      get;
-      private set;
-    }
+        /// <summary>
+        /// Run command action.
+        /// </summary>
+        public Action RunCommand { get; private set; }
 
-    /// <summary>
-    /// Can run command function.
-    /// </summary>
-    public Func<bool> CanRunCommand
-    {
-      get;
-      private set;
-    }
+        /// <summary>
+        /// Can run command function.
+        /// </summary>
+        public Func<bool> CanRunCommand { get; private set; }
 
-    /// <summary>
-    /// Determine if the command can be run.
-    /// </summary>
-    /// <param name="parameter">not used</param>
-    /// <returns>always true</returns>
-    public bool CanExecute(object parameter)
-    {
-      return CanRunCommand();
-    }
+        /// <summary>
+        /// Determine if the command can be run.
+        /// </summary>
+        /// <param name="parameter">not used</param>
+        /// <returns>always true</returns>
+        public bool CanExecute(object parameter)
+        {
+            return this.CanRunCommand();
+        }
 
-    /// <summary>
-    /// Can execute changed event.
-    /// </summary>
-    public event EventHandler CanExecuteChanged
-    {
-      add { CommandManager.RequerySuggested += value; }
-      remove { CommandManager.RequerySuggested -= value; }
-    }
+        /// <summary>
+        /// Can execute changed event.
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
 
-    public void Execute(object parameter)
-    {
-      RunCommand();
-    }
+        public void Execute(object parameter)
+        {
+            this.RunCommand();
+        }
 
-    /// <summary>
-    /// Method which always returns true.
-    /// </summary>
-    /// <returns>always true</returns>
-    private static bool CanAlwaysRunCommand()
-    {
-      return true;
+        /// <summary>
+        /// Method which always returns true.
+        /// </summary>
+        /// <returns>always true</returns>
+        private static bool CanAlwaysRunCommand()
+        {
+            return true;
+        }
     }
-  }
 }
